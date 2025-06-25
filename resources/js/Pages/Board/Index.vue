0afa128d7e4c3e61024b9983dcc800a27518ff38
HJYang <laira241@gmail.com>
2025-06-25 10:50:28 +0900

    - PrimeVue 동작 확인 - board/Index 테이블 추가

--- resources/js/Pages/Board/Index.vue
+++ resources/js/Pages/Board/Index.vue
@@ -1,12 +1,24 @@
 
 <template>
-    <div class="card flex justify-center">
-        <Checkbox v-model="checked" binary />
+    <div class="card">
+        <DataTable :value="products" tableStyle="min-width: 50rem">
+            <Column v-for="col of columns" :key="col.field" :field="col.field" :header="col.header"></Column>
+        </DataTable>
     </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-import Checkbox from 'primevue/checkbox';
-const checked = ref(false);
+import { ref, onMounted } from 'vue';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+onMounted(() => {
+});
+
+const products = ref();
+const columns = [
+    { field: 'title', header: '제목' },
+    { field: 'author', header: '작성자' },
+    { field: 'editDate', header: '마지막 수정일' }
+];
+
 </script>
