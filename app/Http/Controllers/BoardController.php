77f7be5a2988ada2440c32c43014e4a4bb0b3784
HJYang <laira241@gmail.com>
2025-06-26 09:15:16 +0900

    -board model 생성 -migration 생성

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\BoardResource;
 use App\Models\Board;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -9,16 +10,27 @@ use Inertia\Inertia;
 class BoardController extends Controller
 {
     public static function index() {
-        return Inertia::render('Board/Index');
-    }
-
-    public static function getAllBoards() {
-
         $boards = Board::where('isDeleted',false)
                     ->orderBy('created_at','desc')
                     ->get();
         
         return response()-> json($boards);
+        return Inertia::render('Board/Index');
+    }
+
+    public static function getAllBoards() {
+
+        
     
+    }
+
+    public function store(Request $request){
+        $board = Board::create([
+            'title' => $request['title'],
+            'author' => $request['author'],
+            'content' => $request['content'],
+        ]);
+
+        return new BoardResource($board);
     }
 }
