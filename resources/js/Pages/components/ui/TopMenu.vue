c3aff1d80f082e17ad6b79d9a8e3c460415f0fa0
HJYang <laira241@gmail.com>
2025-08-08 09:56:31 +0900

    - 사용자 메뉴 분리

--- resources/js/Pages/components/ui/TopMenu.vue
+++ resources/js/Pages/components/ui/TopMenu.vue
@@ -16,30 +16,7 @@
                     Login
                 </Link>
                 <div v-else>
-                    <Popover>
-                        <PopoverTrigger as-child >
-                            <Button variant="ghost">
-                                {{ page.props.auth.user.name }}
-                            </Button>
-                        </PopoverTrigger>
-                        <PopoverContent side="bottom"  class="w-30 right-0">
-                            <div class="grid gap-4">
-                                <div class="grid gap-2">
-                                    <div class="grid grid-cols-3 items-center gap-4">
-                                        <!-- TODO: 사용자 정보 수정 페이지 추가 -->
-                                        <Link>
-                                            Profile
-                                        </Link>
-                                    </div>
-                                    <div class="grid grid-cols-3 items-center gap-4">
-                                        <Button variant="link" class="ml-2" @click="logout">
-                                            Logout
-                                        </Button>
-                                    </div>  
-                                </div>
-                            </div>                           
-                        </PopoverContent>
-                    </Popover>                    
+                    <Usermenu />
                 </div>
             </div>
         </nav>
@@ -47,13 +24,8 @@
 </template>
 
 <script setup>
-import { Link, usePage, router } from '@inertiajs/vue3';
-import { Button } from "@/Components/ui/button";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/Components/ui/popover"
+import { Link, usePage, } from '@inertiajs/vue3';
+import Usermenu from './Usermenu.vue';
 
 const props = defineProps({
     menus: {
@@ -62,14 +34,4 @@ const props = defineProps({
 });
 const page =usePage();
 
-const logout = () => {
-    router.post('/auth/logout', {}, {
-        onError: (errors) => {
-            console.error('Logout 실패:', errors)
-        },
-        onSuccess: () => {
-            console.log('Logout 성공')
-        }
-    })
-}
 </script>
