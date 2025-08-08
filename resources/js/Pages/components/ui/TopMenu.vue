aac8e9d9421a8d495cb421692a67c0f78a66c8b4
HJYang <laira241@gmail.com>
2025-08-08 09:21:44 +0900

    - 로그인 시 상단 menu에 사용자 설정 추가 (진행중)

--- resources/js/Pages/components/ui/TopMenu.vue
+++ resources/js/Pages/components/ui/TopMenu.vue
@@ -12,19 +12,48 @@
                 </Link>
             </div>
             <div>
-                <Link href="/auth/login">
+                <Link v-if="!page.props.auth.user" href="/auth/login">
                     Login
                 </Link>
+                <div v-else>
+                    <Popover>
+                        <PopoverTrigger as-child >
+                            <Button variant="ghost">
+                                {{ page.props.auth.user.name }}
+                            </Button>
+                        </PopoverTrigger>
+                        <PopoverContent side="bottom"  class="w-30 right-0">
+                            <Link :href="/auth/logout">
+                                Logout
+                            </Link>
+                            <Link>
+                                Profile
+                            </Link>
+                        </PopoverContent>
+                    </Popover>
+                    
+                </div>
             </div>
         </nav>
     </div>
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/vue3';
+import { Link, usePage, } from '@inertiajs/vue3';
+import { Button } from "@/Components/ui/button";
+import { Input } from "@/Components/ui/input"
+import { Label } from "@/Components/ui/label"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/Components/ui/popover"
+
 const props = defineProps({
     menus: {
         type: Object
     }
 });
+const page =usePage();
+
 </script>
